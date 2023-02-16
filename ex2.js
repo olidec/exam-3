aa65711817a98629dcb00159ae4de49312fbb73c
Nicolas <e272678@sbl.ch>
2023-02-16 13:27:07 +0100

    exercise2f

---
+++ ex2.js
@@ -0,0 +1,6 @@
+const numbers = ["45", "12", "56", "23", "34", "67", "78", "89"];
+ <p class="6thEntry"></p>
+
+ <script>
+    document.getElementByClassName("6th HTML").innerHTML = "67";
+ </script>
