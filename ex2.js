a8007eb792aa9746f390d746eb76cde742f4b1c2
SnowWhiteOnE227600 <113612334+SnowWhiteOnE227600@users.noreply.github.com>
2023-02-16 13:57:48 +0100

    Exercise 1

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let a = [45, 12, 56, 23, 34, 67, 78, 89];
+
+//console.log(a.length)
+//console.log(a[3] = 27)
+//console.log(a.push(42))
+//console.log(a.reverse().pop())
+
+//let arr = a[a.length - 1];
+//console.log(arr);
+
