8ac4a1124118a764be7fa26423b88294dbfce369
Nicolascado <e226593@sbl.ch>
2023-02-16 13:24:26 +0100

    Bye

---
+++ ex2.js
@@ -0,0 +1,16 @@
+const b = [54,21,65,32,43,76,87,98];
+
+let length = b.length;
+document.getElementById("Numbers").innerHTML = b.length;
+
+
+b[3] = 59;
+document.getElementById("ex2").innerHTML = b;
+
+b.push(22);
+document.getElementById("ex3").innerHTML = b;
+
+document.getElementById("ex4").innerHTML = b.sort().pop();
+
+const o = ['i am finished']
+document.getElementById("ex2f").innerHTML = o;
