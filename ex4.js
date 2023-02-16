f9d7dd7753e220b890d6c7e9441d49bbaa5b68e6
janinibanini10 <e236330@sbl.ch>
2023-02-16 14:04:20 +0100

    erafdy

--- ex4.js
+++ ex4.js
@@ -5,11 +5,7 @@ var draw = function()
     if (mouseIsPressed) {
         xPositions.push(mouseX);
     }
-    noStroke();
-    background(212, 254, 255);
-    
-    stroke(64, 117, 207);
-    fill(196, 33, 255);
+
     for (var i = 0; i < xPositions.length; i++) { 
         var hopper = getImage("tinkerbell");
     image(hopper, 189, 227);
