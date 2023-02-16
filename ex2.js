1f96f3cec352771571c567ef5cc5019937953c29
Klaus-XVI <e272700@sbl.ch>
2023-02-16 13:58:31 +0100

    ......

--- ex2.js
+++ ex2.js
@@ -1,3 +1,7 @@
 let b = [54,21,65,32,43,76,87,98];
 
-document.getElementById('ex2f').innerHTML = b[5];
+document.getElementById("ex2f").innerHTML = b[5]
+
+
+document.getElementById("tinkerbell")
+ctx.drawImage(tinkerbell)
