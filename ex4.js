8601f4ba6455f24dbcb20176c1b8cea01724353a
Scott-Calvin <e272695@edu.sbl.ch>
2023-02-16 14:03:44 +0100

    my second changes for ex4

---
+++ ex4.js
@@ -0,0 +1,12 @@
+
+
+    function m(e){ 
+        var bx = document.getElementById("tinkerbell"); 
+        bx.style.left = e.pageX; 
+        bx.style.top = e.pageY; 
+        onmousemove="m(event)" 
+    
+    }
+
+       
+		
